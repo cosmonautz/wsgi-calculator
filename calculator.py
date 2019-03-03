a7a8e59067afb162a5d515da4cc446db8f83d445
Kevin Waldron <kevinmwaldron@gmail.com>
2019-03-03 09:04:36 -0800

    my changes

--- calculator.py
+++ calculator.py
@@ -40,45 +40,89 @@ To submit your homework:
 
 
 """
-
+import traceback
+from functools import reduce 
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    total = sum(map(int, args))
+    return str(total)
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
+def subtract(*args):
+    """ Returns a STRING with the subtraction of the arguments """
+    total = reduce((lambda x, y: x - y), map(int, args)) 
+    return str(total)
 
-    return sum
+def multiply(*args):
+    """ Returns a STRING with the multiplication of the arguments """
+    total = reduce((lambda x, y: x * y), map(int, args)) 
+    return str(total)
 
-# TODO: Add functions for handling more arithmetic operations.
+def divide(*args):
+    """ Returns a STRING with the division of the arguments """
+    total = reduce((lambda x, y: x / y), map(int, args)) 
+    return str(total)
+
+def howto(*args):
+    """ Returns a STRING with the howto page """
+    page = """
+<h1>How to use the calculator</h1>
+<body>
+To add append /add/arg1/arg2/.../argn to the end of the page url<br>
+To subtract append /subtract/arg1/arg2/.../argn to the end of the page url<br>
+To multiply append /multiply/arg1/arg2/.../argn to the end of the page url<br>
+To divide append /divide/arg1/arg2/.../argn to the end of the page url<br>
+<br>
+Examples:<br>
+ http://localhost:8080/multiply/3/5   => 15<br>
+  http://localhost:8080/add/23/42      => 65<br>
+  http://localhost:8080/subtract/23/42 => -19<br>
+  http://localhost:8080/divide/22/11   => 2<br>
+</body>
+    """
+    return page
 
 def resolve_path(path):
     """
     Should return two values: a callable and an iterable of
     arguments.
     """
-
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {'': howto, 'add':add, 'subtract':subtract, 'multiply':multiply, 'divide':divide}
+    path = path.strip('/').split('/')
+    func_name = path[0]
+    args = path[1:]
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = '200 OK'
+    except NameError:
+        status = '404 Not Found'
+        body = '<h1>Not Found</h1>'
+    except ZeroDivisionError:
+        status = '500 Divide By Zero'
+        body = '<h1>Divide By Zero</h1>'
+    except Exception:
+        status = '500 Internal Server Error'
+        body = '<h1>Internal Server Error</h1>'
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
